78fc1806929b945ef25b4b1fcef23be4e76ffea7
Dohyun Kim <dkmqflx@gmail.com>
2022-09-27 13:31:50 +0900

    feat: 로딩효과 적용하기

--- js/main.js
+++ js/main.js
@@ -855,12 +855,17 @@
   window.addEventListener('orientationchange', setLayoyt);
 
   window.addEventListener('load', () => {
+    document.body.classList.remove('before-load');
     setLayoyt; // 새로고침 했을 때 처리해주기 위함
     sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
   });
   // DOMContentLoaded는 load가 이미지 파일들을 포함해서 모든 파일이 로드되면 실행되는 것고 ㅏ달리
   // html 객체들만 로드가 끝나면 바로 실행된다
   // 따라서 실행시점이 더 빠르다는 장점 있다
+
+  document.querySelector('.loading').addEventListener('transitionend', (e) => {
+    document.body.removeChild(e.currentTarget);
+  });
 
   setCanvasImage();
 })();
